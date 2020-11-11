21913c0bda61f71fd6c2a8cde430f9a3782b0a39
Andres323-byte <nelsonandres0323@gmail.com>
2020-11-10 20:34:19 -0500

    login 2 parte

---
+++ parte1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<?php require_once('partes/head.php'); ?>
+<body  class="text-center">
+
+<?php require_once('partes/login.php'); ?>
+
+
+<?php require_once('partes/scripts.php'); ?>
+</body>
+</html>
