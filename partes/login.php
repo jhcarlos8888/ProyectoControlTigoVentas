e589a3f27ae42895158e63b9f201cb3f363fbb06
Juan Carlos Herrera Blandon <jhcarlos181@protonmail.com>
2020-11-15 18:09:25 -0500

    Creation plantilla.php and modified login.php

--- partes/login.php
+++ partes/login.php
@@ -1,3 +1,4 @@
+<div id="bodyForm">
 <form class="form-signin" id="login">
   <img class="mb-4" src="img/tigo-logo-11.png" alt="" width="200" height="200">
   <h1 class="h3 mb-3 font-weight-normal">TIGO CONTROL</h1>
@@ -11,3 +12,4 @@
   <button class="btn btn-lg btn-primary btn-block" type="submit" name="boton">Ingresar</button>
   <p class="mt-5 mb-3 text-muted">&copy;2020</p>
 </form>
+</div>
--- partes/scripts.php
+++ partes/scripts.php
@@ -1,3 +1,10 @@
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+
+
+
 <script src="js/dashboard.js"></script>
