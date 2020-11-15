2808955407a334a3b45484a7dc962fafb99979f4
Juan Carlos Herrera Blandon <jhcarlos181@protonmail.com>
2020-11-15 16:38:02 -0500

    changes on login.php

--- partes/login.php
+++ partes/login.php
@@ -1,13 +1,13 @@
 <form class="form-signin">
   <img class="mb-4" src="img/tigo-logo-11.png" alt="" width="200" height="200">
   <h1 class="h3 mb-3 font-weight-normal">TIGO CONTROL</h1>
-  <input type="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
-  <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
+  <input type="email" id="email" name ="email" class="form-control" placeholder="Email" required autofocus>
+  <input type="password" id="clave" name="clave" class="form-control" placeholder="Contraseña" required>
   <div class="checkbox mb-3">
     <label>
-      <input type="checkbox" value="remember-me"> Recuérdame
+      <input type="checkbox" value="remember-me" required="required"> Recuérdame
     </label>
   </div>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Ingresar</button>
+  <button class="btn btn-lg btn-primary btn-block" type="submit" name="boton">Ingresar</button>
   <p class="mt-5 mb-3 text-muted">&copy;2020</p>
 </form>
