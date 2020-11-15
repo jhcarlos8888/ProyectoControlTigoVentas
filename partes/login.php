76130c7373cab23fe010e894e8b690b391c0bf4e
Juan Carlos Herrera Blandon <jhcarlos181@protonmail.com>
2020-11-15 16:51:59 -0500

    login part ultimate changes

--- partes/login.php
+++ partes/login.php
@@ -1,4 +1,4 @@
-<form class="form-signin">
+<form class="form-signin" id="login">
   <img class="mb-4" src="img/tigo-logo-11.png" alt="" width="200" height="200">
   <h1 class="h3 mb-3 font-weight-normal">TIGO CONTROL</h1>
   <input type="email" id="email" name ="email" class="form-control" placeholder="Email" required autofocus>
