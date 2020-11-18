2d56ec57ac9576ab6751c2cae64dbc2c9d3a4bd4
Faver Lopez <faverl@misena.edu.co>
2020-11-18 12:26:32 -0500

    Se modifica el action del formulario de login para que redireccione a la plantilla

--- partes/login.php
+++ partes/login.php
@@ -1,5 +1,5 @@
 <div id="bodyForm">
-<form class="form-signin" id="login">
+<form class="form-signin" id="login" method="post" action="plantilla.php">
   <img class="mb-4" src="img/tigo-logo-11.png" alt="" width="200" height="200">
   <h1 class="h3 mb-3 font-weight-normal">TIGO CONTROL</h1>
   <input type="email" id="email" name ="email" class="form-control" placeholder="Email" required autofocus>
