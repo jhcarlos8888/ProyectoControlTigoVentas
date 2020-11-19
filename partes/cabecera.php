cdf12e50bc984adf308f4a9bd8e2b6cd3770ae3c
Faver Lopez <faverl@misena.edu.co>
2020-11-18 19:20:57 -0500

    Se agrega direccionamiento al boton de cerrar sesion de la cabecera

--- partes/cabecera.php
+++ partes/cabecera.php
@@ -7,7 +7,7 @@
   <input class="form-control form-control-dark w-100" type="text" placeholder="Buscar" aria-label="Search">
   <ul class="navbar-nav px-3">
     <li class="nav-item text-nowrap">
-      <a class="nav-link" href="#">Cerrar Sesión</a>
+      <a class="nav-link" href="index.php">Cerrar Sesión</a>
     </li>
   </ul>
 </nav>
