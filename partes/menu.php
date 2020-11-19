5a421204caad0877f553dd5094f12aeae2cca294
Juan Carlos Herrera Blandon <jhcarlos181@protonmail.com>
2020-11-18 21:43:24 -0500

    submenu intems created in menu part

--- partes/menu.php
+++ partes/menu.php
@@ -7,34 +7,46 @@
               Inicio<span class="sr-only">(current)</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="bar-chart-2"></span>
-              Seguimientos
-            </a>
+          
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><span data-feather="bar-chart-2"></span>Seguimientos</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Seguimiento de clientes</a>
+                <a class="dropdown-item" href="#">Actualización de clientes</a>
+                <a class="dropdown-item" href="#">Ventas, productos y servicios</a>
+                <a class="dropdown-item" href="#">Envios de SMS</a>
+                <a class="dropdown-item" href="#">Generacion de Id de pedido</a>
+              </div>
+          </li>
+         <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><span data-feather="dollar-sign"></span>Financiera</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Descuentos y promociones</a>
+                <a class="dropdown-item" href="#">Historial de ventas</a>
+                <a class="dropdown-item" href="#">Estrategias de ventas</a>
+                <a class="dropdown-item" href="#">Generación de informes financieros</a>
+                <a class="dropdown-item" href="#">Indicadores de gestión comercial</a>
+              </div>
           </li>
 
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="dollar-sign"></span>
-              Financiera
-            </a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="users"></span>
-              Usuarios
-            </a>
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><span data-feather="users"></span>Usuarios</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Ingreso a mi perfil</a>
+                <a class="dropdown-item" href="#">Cambio de contraseña</a>
+                <a class="dropdown-item" href="#">Desactivacion de usuarios</a>
+                <a class="dropdown-item" href="#">Rol de usuarios</a>
+              </div>
           </li>
           
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="layers"></span>
-              Ayudas
-            </a>
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><span data-feather="layers"></span>Ayudas</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Diseño volante publicitario</a>
+                <a class="dropdown-item" href="#">Manuales tecnicos y de uso</a>
+                <a class="dropdown-item" href="#">Documentos legales</a>
+              </div>
           </li>
-        </ul>
 
       </div>
     </nav>
