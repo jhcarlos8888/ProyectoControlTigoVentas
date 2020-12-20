9a46639a9b386f282ca52e4dfc3e80f9f31d623b
Faver Lopez <faverl@misena.edu.co>
2020-12-20 04:13:55 -0500

    Se modifica formulario login, se elimina el requerido del campo recordarme, se agrega un campo oculto para enviar al controlador que debe realizar al recibir peticion del formulario login

--- partes/login.php
+++ partes/login.php
@@ -3,19 +3,20 @@
     session_start();
 
     ?>
-<form class="form-signin" id="login" method="post" action="plantilla.php">
-  <img class="mb-4" src="img/tigo-logo-11.png" alt="" width="200" height="200">
-  <h1 class="h3 mb-3 font-weight-normal">TIGO CONTROL</h1>
-  <input type="email" id="email" name ="email" class="form-control" placeholder="Email" required autofocus>
-  <input type="password" id="clave" name="clave" class="form-control" placeholder="Contraseña" required>
-  <div class="checkbox mb-3">
-    <label>
-      <input type="checkbox" value="remember-me" required="required"> Recuérdame
-    </label>
-  </div>
-  <button class="btn btn-lg btn-primary btn-block" type="submit" name="boton">Ingresar</button>
-  <p class="mt-5 mb-3 text-muted">&copy;2020</p>
-</form>
+    <form class="form-signin" id="login" method="post" action="controladores/controlador.php">
+        <input type="hidden" name="proceso" value="login">
+        <img class="mb-4" src="img/tigo-logo-11.png" alt="" width="200" height="200">
+        <h1 class="h3 mb-3 font-weight-normal">TIGO CONTROL</h1>
+        <input type="email" id="email" name="email" class="form-control" placeholder="Email" required autofocus>
+        <input type="password" id="clave" name="clave" class="form-control" placeholder="Contraseña" required>
+        <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="remember-me"> Recuérdame
+            </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="boton">Ingresar</button>
+        <p class="mt-5 mb-3 text-muted">&copy;2020</p>
+    </form>
 </div>
 
 <?php
