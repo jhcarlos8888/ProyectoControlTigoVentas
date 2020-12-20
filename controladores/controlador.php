cfeb03e6c9be9adc27ea0c19564c7d799dc0a49c
Faver Lopez <faverl@misena.edu.co>
2020-12-20 04:11:51 -0500

    Agrego carpeta controlador y se adiciona clase controlador que recibira peticiones y las procesara

---
+++ controladores/controlador.php
@@ -0,0 +1,35 @@
+<?php
+
+require __DIR__ . '/../modelo/Usuario.php';
+
+$proceso = (isset($_POST['proceso'])) ? $_POST['proceso'] : "index";
+
+switch ($proceso) {
+
+    case "index":
+        header("Location: ../index.php");
+        die();
+
+    case "login":
+
+        $email = $_POST['email'];
+        $contrasena = $_POST['clave'];
+
+        $usuario = new Usuario();
+
+        if ($usuario->ConsultarUsuario($email)) {
+
+            if (strcmp($usuario->getContrasena(), $contrasena) == 0) {
+                //header("Location: ../plantilla.php");
+                $usuario->getNombre();
+                die();
+            }else{
+                header("Location: ../index.php");
+                die();
+            }
+        } else {
+            header("Location: ../index.php");
+            die();
+        }
+
+}
