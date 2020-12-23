14564e7ff1bffbfab66bef94c76b2dd7d1605364
Faver Lopez <faverl@misena.edu.co>
2020-12-23 13:37:29 -0500

    Se agrega la carpeta de vistas y se agrega carpeta de vista para el modelo de usuario

--- vistas/partes/login.php
+++ vistas/partes/login.php
@@ -1,7 +1,7 @@
 <div id="bodyForm">
-    <form class="form-signin" id="login" method="post" action="controladores/controlador.php">
+    <form class="form-signin" id="login" method="post" action="../aplicacion/controladores/controlador.php">
         <input type="hidden" name="proceso" value="login">
-        <img class="mb-4" src="img/tigo-logo-11.png" alt="" width="200" height="200">
+        <img class="mb-4" src="../img/tigo-logo-11.png" alt="" width="200" height="200">
         <h1 class="h3 mb-3 font-weight-normal">TIGO CONTROL</h1>
         <input type="email" id="email" name="email" class="form-control" placeholder="Email" required autofocus>
         <input type="password" id="clave" name="clave" class="form-control" placeholder="Contraseña" required>
