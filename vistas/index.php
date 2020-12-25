e6b5a981797cf187c50d11e2ea179c60cd2af96c
Faver Lopez <faverl@misena.edu.co>
2020-12-25 12:16:25 -0500

    Se realiza modificacion del archivo index

--- vistas/index.php
+++ vistas/index.php
@@ -1,8 +1,21 @@
 <!DOCTYPE html>
 <html lang="es">
-<?php require_once(RUTA_VISTAS.'partes/head.php'); ?>
+<head>
+    <title>Ingresar</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="css/estilo.css" type="text/css" rel="stylesheet">
+    <link href="css/bootstrap/bootstrap.css" type="text/css" rel="stylesheet">
+</head>
 <body  class="text-center">
-<?php require_once(RUTA_VISTAS.'partes/login.php'); ?>
-<?php require_once(RUTA_VISTAS.'partes/scripts.php'); ?>
+    <?php include(RUTA_VISTAS.'partes/login.php') ?>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+
+    <script src="js/dashboard.js"></script>
 </body>
 </html>
