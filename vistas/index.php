9cdc4f0eb1fde82f0a3840de3597e30688beb807
Faver Lopez <faverl@misena.edu.co>
2020-12-26 12:39:58 -0500

    Se agrega icono para el titulo de la vista index

--- vistas/index.php
+++ vistas/index.php
@@ -2,6 +2,7 @@
 <html lang="es">
 <head>
     <title>Ingresar</title>
+    <link rel="shortcut icon" href="https://www.flaticon.es/svg/static/icons/svg/458/458910.svg" type="image/svg+xml">
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="css/estilo.css" type="text/css" rel="stylesheet">
