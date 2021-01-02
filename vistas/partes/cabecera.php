40f7323d89ca3f9e98437aae0a2785ca1791e35f
Juan <jhcarlos181@protonmail.com>
2021-01-02 15:21:02 -0500

    Se organiza elementos de cabecera para que funcione  en responsive

--- vistas/partes/cabecera.php
+++ vistas/partes/cabecera.php
@@ -4,14 +4,14 @@
 </div>
 
 <nav class="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-8 text-center " href="#"><img src="../img/logo_project_control_min.png" width="50" alt=""> Sales Control System</a>
-
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse"
+    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-8 text-center " href="#"><img src="../img/logo_project_control_min.png" width="50" alt=""></a>
+    <h4 class=" navbar-nav ml-2 px-8 text-center">Sales Control System</h4>
+    <button class="navbar-toggler position-absolute d-dm-none d-lg-none collapsed my-2" type="button" data-toggle="collapse"
             data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <ul class="navbar-nav px-3">
+    <ul class="navbar-nav pr-3 py-1">
         <li class="nav-item text-nowrap">
             <form method="post" action="">
                 <input type="hidden" name="proceso" value="logout">
