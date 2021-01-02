996ea6e38c6386c75dd921ece276d6c4d73426d1
Faver Lopez <faverl@misena.edu.co>
2021-01-02 16:56:22 -0500

    Refactorin de plantilla login
    -- Se elimina la casilla de recordarme
    
    -+ Se cambia el campo de email por cedula
    
    ++ se agrega estilo para cambiar color al boton de ingresar
    
    -+ Se modifica el campo hidden
    
    -+ Se cambia ruta de peticion para loguearse

--- vistas/partes/login.php
+++ vistas/partes/login.php
@@ -1,16 +1,11 @@
 <div id="bodyForm">
-    <form class="form-signin" id="login" method="post" action="usuario/loguin" >
-        <input type="hidden" name="proceso" value="login">
+    <form class="form-signin" id="login" method="post" action="<?php url("autenticacion/login")?>">
+        <input type="hidden" name="token" value="--Aqui va valor de token">
         <img class="mb-4" src="img/logo_project_control_max.png" alt="" width="200" height="200">
         <h1 class="h3 mb-3 font-weight-normal">Sales Control System</h1>
-        <input type="email" id="email" name="email" class="form-control" placeholder="Email" required autofocus>
+        <input type="number" id="cedula" name="cedula" class="form-control" placeholder="Usuario" required autofocus minlength="5" maxlength="12">
         <input type="password" id="clave" name="clave" class="form-control" placeholder="Contraseña" required>
-        <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="remember-me"> Recuérdame
-            </label>
-        </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit" name="boton">Ingresar</button>
+        <button class="btn btn-lg btn-primary btn-scs btn-block" type="submit" name="boton">Ingresar</button>
         <p class="mt-5 mb-3 text-muted">&copy;2020</p>
     </form>
 </div>
