2ffa324532664a396274bd7bb5725ba3a7cc5e87
Faver Lopez <faverl@misena.edu.co>
2021-01-02 14:06:52 -0500

    Se crea ControladorAutenticacion
    -- Se copia la funcion "loguin" contenida en ControladorUsuario y se
    pega en este controlador.
    
    -- Se renombre la funcion "loguin" por "login"

---
+++ aplicacion/controladores/ControladorAutenticacion.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class ControladorAutenticacion
+{
+
+    public function login()
+    {
+        $cedula = $_POST['cedula'];
+        $contrasena = $_POST['clave'];
+
+        $usuario = new Usuario();
+
+        if ($usuario->LoguearUsuario($cedula, $contrasena)) {
+
+            $listaUsuarios = $usuario->ListarUsuarios();
+
+            return Vista::crear("usuario.ListarUsuarios", "listaUsuarios", $listaUsuarios);
+        } else {
+
+            $urlprin = str_replace("index.php", "", $_SERVER["PHP_SELF"]);
+
+            header("location:/" . trim($urlprin, "/") . "" . "");
+        }
+    }
+}
