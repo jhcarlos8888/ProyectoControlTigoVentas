53519d5992ab5ab4e15cfc0439f893d4d7a8b91b
Juan Carlos <jhcarlos181@protonmail.com>
2021-01-05 10:55:35 -0500

    Cambio de secuencia en archivos css, eliminacion de logo tigo

--- vistas/index.php
+++ vistas/index.php
@@ -2,11 +2,14 @@
 <html lang="es">
 <head>
     <title>Ingresar</title>
+	
     <link rel="shortcut icon" href="https://www.flaticon.es/svg/static/icons/svg/458/458910.svg" type="image/svg+xml">
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="css/estilo.css" type="text/css" rel="stylesheet">
-    <link href="css/bootstrap/bootstrap.css" type="text/css" rel="stylesheet">
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link href="css/bootstrap/bootstrap.css" type="text/css" rel="stylesheet">
+	<link href="css/estilo.css" type="text/css" rel="stylesheet">
+	
+	
 </head>
 <body  class="text-center">
     <?php include(RUTA_VISTAS.'partes/login.php') ?>
--- vistas/partes/head.php
+++ vistas/partes/head.php
@@ -3,3 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link href="../css/bootstrap/bootstrap.css" type="text/css" rel="stylesheet">
 <link href="../css/estilo.css" type="text/css" rel="stylesheet">
+
+
+
+
