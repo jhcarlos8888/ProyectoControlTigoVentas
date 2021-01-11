84b4f258c1157da883b9bc45f5802ae00347fd8e
Juan Carlos <jhcarlos181@protonmail.com>
2021-01-11 15:25:11 -0500

    Se Organiza clase accordion en la lista del menu

--- vistas/partes/menu.php
+++ vistas/partes/menu.php
@@ -1,7 +1,7 @@
 <nav id="sidebarMenu" class="col-12 col-md-3  d-md-block bg-light sidebar collapse text-md-center text-left mr-0 pr-0 py-md-5 px-0">
       <div class="sidebar-sticky p-10">
 
-        <ul class="nav flex-column" id="accordionExample">
+        <ul class="nav flex-column accordion" id="accordionExample">
 
 
                       <li class="nav-item">
@@ -11,57 +11,57 @@
                         </a>
                       </li>
 
-                      <li class="nav-item dropdow accordion">
+                      <li class="nav-item dropdow">
 
                           <a class="nav-link dropdown-toggle" role="button" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne"><span data-feather="bar-chart-2"></span>Seguimientos</a>
                           
                           <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
-                            <a class="dropdown-item pr-1" href="#">Seguimiento de clientes</a>
-                            <a class="dropdown-item pr-1" href="#">Actualización de clientes</a>
-                            <a class="dropdown-item pr-1" href="#">Ventas, productos y servicios</a>
-                            <a class="dropdown-item pr-1" href="#">Envios de SMS</a>
-                            <a class="dropdown-item pr-1" href="#">Generacion de Id de pedido</a>
+                            <a class="dropdown-item" href="#">Seguimiento de clientes</a>
+                            <a class="dropdown-item" href="#">Actualización de clientes</a>
+                            <a class="dropdown-item" href="#">Ventas, productos y servicios</a>
+                            <a class="dropdown-item" href="#">Envios de SMS</a>
+                            <a class="dropdown-item" href="#">Generacion de Id de pedido</a>
                           </div>
                       </li>
 
-                    <li class="nav-item dropdown accordion">
+                    <li class="nav-item dropdown">
 
                           <a class="nav-link dropdown-toggle" role="button" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"><span data-feather="dollar-sign"></span>Mi gestión</a>
                           
                           <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                            <a class="dropdown-item pr-1" href="#">Descuentos y promociones</a>
-                            <a class="dropdown-item pr-1" href="#">Historial de ventas</a>
-                            <a class="dropdown-item pr-1" href="#">Estrategias de ventas</a>
-                            <a class="dropdown-item pr-1" href="#">Generación de informes financieros</a>
-                            <a class="dropdown-item pr-1" href="#">Indicadores de gestión comercial</a>
+                            <a class="dropdown-item" href="#">Descuentos y promociones</a>
+                            <a class="dropdown-item" href="#">Historial de ventas</a>
+                            <a class="dropdown-item" href="#">Estrategias de ventas</a>
+                            <a class="dropdown-item" href="#">Generación de informes financieros</a>
+                            <a class="dropdown-item" href="#">Indicadores de gestión comercial</a>
                           </div>
                       </li>
 
 
-                      <li class="nav-item dropdown accordion">
+                      <li class="nav-item dropdown">
 
                           <a class="nav-link dropdown-toggle" role="button" href="#" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"><span data-feather="users"></span>Usuarios</a>
                           
                           <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                            <a class="dropdown-item pr-1" href="#">Ingreso a mi perfil</a>
-                            <a class="dropdown-item pr-1" href="#">Cambio de contraseña</a>
-                            <a class="dropdown-item pr-1" href="#">Desactivacion de usuarios</a>
-                            <a class="dropdown-item pr-1" href="#">Rol de usuarios</a>
+                            <a class="dropdown-item" href="#">Ingreso a mi perfil</a>
+                            <a class="dropdown-item" href="#">Cambio de contraseña</a>
+                            <a class="dropdown-item" href="#">Desactivacion de usuarios</a>
+                            <a class="dropdown-item" href="#">Rol de usuarios</a>
                           </div>
                       </li>
 
 
 
-                      <li class="nav-item dropdown accordion">
+                      <li class="nav-item dropdown">
 
 
                           <a class="nav-link dropdown-toggle" role="button" href="#" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour"><span data-feather="layers"></span>Ayudas</a>
 
 
                           <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
-                            <a class="dropdown-item pr-1" href="#">Diseño volante publicitario</a>
-                            <a class="dropdown-item pr-1" href="#">Manuales tecnicos y de uso</a>
-                            <a class="dropdown-item pr-1" href="#">Documentos legales</a>
+                            <a class="dropdown-item" href="#">Diseño volante publicitario</a>
+                            <a class="dropdown-item" href="#">Manuales tecnicos y de uso</a>
+                            <a class="dropdown-item" href="#">Documentos legales</a>
 
                           </div>
 
