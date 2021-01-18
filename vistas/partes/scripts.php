46c4eec3fa6609199017bdd6ad8de916371bed4a
Faver Lopez <faverl@misena.edu.co>
2021-01-17 22:05:54 -0500

    Se eliminan script que no se estan usando y se agrega nuevo script scs.js

--- vistas/partes/scripts.php
+++ vistas/partes/scripts.php
@@ -4,6 +4,5 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
         crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
-<script src="<?php assets("js/dashboard.js") ?>"></script>
+<script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
+<script src="<?php assets("js/scs.js") ?>"></script>
