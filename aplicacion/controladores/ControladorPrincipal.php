a16a392a7b9958c092943a5d394cfee6a7b5f54a
Faver Lopez <faverl@misena.edu.co>
2021-02-07 13:51:48 -0500

    Se realiza de la implementación del redireccionamiento en el index del controlador

--- aplicacion/controladores/ControladorPrincipal.php
+++ aplicacion/controladores/ControladorPrincipal.php
@@ -6,8 +6,6 @@ class ControladorPrincipal
 {
     public function index(){
 
-        $urlprin = str_replace("index.php", "", $_SERVER["PHP_SELF"]);
-
-        header("location:/" . trim($urlprin, "/") . "/autenticacion");
+        redirecciona("autenticacion");
     }
 }
