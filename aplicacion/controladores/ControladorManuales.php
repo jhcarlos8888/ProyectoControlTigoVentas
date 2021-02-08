47f832ab0f9f9c62815166d26809abe1940cd845
Faver Lopez <faverl@misena.edu.co>
2021-02-07 21:15:48 -0500

    Se realiza implementacion para el manejo de manuales

--- aplicacion/controladores/ControladorManuales.php
+++ aplicacion/controladores/ControladorManuales.php
@@ -1,16 +1,43 @@
 <?php
 
-
+use manual\Manual;
 use vista\Vista;
 
 class ControladorManuales
 {
 
-    public function index(){
-
+    public function index()
+    {
         validarSession();
 
-        return Vista::crear("manual.ListarManuales");
+        $nuevoManual = new Manual();
+
+        $listaDemanuales = $nuevoManual->listarManuales();
+
+        return Vista::crear("manual.ListarManuales", "listaDeManuales", $listaDemanuales);
+    }
+
+    public function subir()
+    {
+        validarSession();
+
+        return Vista::crear("manual.registrar");
+    }
+
+    public function crear()
+    {
+        validarSession();
+
+        $nombre = $_POST['nombre'];
+        $archivo = $_FILES['manual'];
+        $rutaMelodia = $_FILES['melodia']['tmp_name'];
+
+        $nuevoManual = new Manual();
+
+        $nuevoManual->setNombre($nombre);
+        $nuevoManual->setRuta($rutaMelodia);
+
+        
     }
 
 }
---
+++ aplicacion/modelo/Manual.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace manual;
+
+use Conexion;
+use PDO;
+
+class Manual
+{
+    private $id;
+    private $nombre;
+    private $ruta;
+
+    public function getId()
+    {
+        return$this->id;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function getRuta()
+    {
+        return$this->ruta;
+    }
+
+    public function setId($id)
+    {
+        $this->nombre = $id;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function setRuta($ruta)
+    {
+        $this->ruta = $ruta;
+    }
+
+    public function listarManuales()
+    {
+        $listaDeManuales = array();
+        $conexionDataBase = new Conexion();
+        $conexion = $conexionDataBase->CrearConexion();
+        $stmt = $conexion->prepare("SELECT * FROM manual");
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+        $stmt->execute();
+
+        while ($fila = $stmt->fetch()) {
+
+            $nuevoManual = new Manual();
+
+            $nuevoManual->setId($fila->id);
+            $nuevoManual->setNombre($fila->nombre);
+            $nuevoManual->setRuta($fila->ruta);
+
+            $listaDeManuales[] = $nuevoManual;
+        }
+        return $listaDeManuales;
+    }
+}
