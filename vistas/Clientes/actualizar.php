f02d4a43d7ef362c8f92dea5ffbfdaf220bb1bd2
Juan Carlos <jhcarlos181@protonmail.com>
2021-02-16 21:20:48 -0500

    Se modifican estilos de actualizar cliente

--- vistas/Clientes/actualizar.php
+++ vistas/Clientes/actualizar.php
@@ -15,90 +15,64 @@
 
         <?php include(RUTA_VISTAS . 'partes/menu.php') ?>
 
-        <main role="main" class="col-12 col-md-9 ml-sm-auto  px-md-4 py-md-5 py-2 ">
-
+        <main role="main" class="col-12 col-md-10 px-md-4 py-md-5 py-2">
             <div class="text-center">
-
-                <img class="mb-4 img-fluid" src="<?php assets("img/edit_user.png") ?>" alt="" width="100" height="100">
-
-                <h3>ACTUALIZAR CLIENTE</h3>
-
+                <img class="mb-4 img-fluid" src="<?php assets("img/user_bussines.png") ?>" alt="" width="60"
+                     height="60">
+                <h3 class="alert-secondary">ACTUALIZAR CLIENTE</h3>
             </div>
 
-            <form class="form " id="login" method="post" action="<?php url("cliente/actualizar") ?>">
+            <form class="row" id="registrar" method="post" action="<?php url("cliente/actualizar") ?>">
+
                 <input type="hidden" name="proceso" value="login"><!--Aqui debe ir un token-->
                 <input type="hidden" name="id" value="<?= $cliente->__get("id")?>">
 
-                <div class=" form-group row offset-md-2 my-0">
-                    <div class="col-md-3">
-                        <label for="identificacion" class="col-form-label my-2">IDENTIFICACIÓN</label>
-                    </div>
-                    <div class="col-md-5">
-                        <input type="text" name="identificacion" id="identificacion"
-                               value="<?= $cliente->__get("identificacion") ?>"
-                               class="form-control text-center my-2" placeholder="Ingrese cedula del cliente">
-                    </div>
+                <div class="col-md-6">
+                    <label for="nombre" class="col-form-label">Nombre</label>
+                    <input type="text" name="nombre" id="nombre" class="form-control text-capitalize"
+                           value="<?= $cliente->__get("nombre")?>" placeholder="Ingrese Nombre Completo" required>
                 </div>
 
-                <div class=" form-group row offset-md-2 my-0">
-                    <div class="col-md-3">
-                        <label for="nombre" class="col-form-label my-2">NOMBRE</label>
-                    </div>
-                    <div class="col-md-5">
-                        <input type="text" name="nombre" id="nombre" class="form-control text-center my-2"
-                               value="<?= $cliente->__get("nombre")?>"
-                               placeholder="Ingrese nombre del cliente">
-                    </div>
+                <div class="col-md-6">
+                    <label for="identificacion" class="col-form-label">Identificacion</label>
+                    <input type="number" name="identificacion" id="identificacion" class="form-control"
+                           value="<?= $cliente->__get("identificacion") ?>" placeholder="Ingrese Cedula" required>
                 </div>
 
-                <div class=" form-group row offset-md-2 my-0">
-                    <div class="col-md-3">
-                        <label for="celular" class="col-form-label my-2">CELULAR</label>
-                    </div>
-                    <div class="col-md-5">
-                        <input type="text" name="celular" id="celular" class="form-control text-center my-2"
-                               value="<?= $cliente->__get("celular")?>"
-                               placeholder="Ingrese celular">
-                    </div>
+
+                <div class="col-md-6">
+                    <label for="email" class="col-form-label">Email</label>
+                    <input type="email" name="email" id="email" value="<?= $cliente->__get("email")?>" class="form-control" placeholder="Ingrese Correo"
+                           required>
                 </div>
 
-                <div class=" form-group row offset-md-2 my-0">
-                    <div class="col-md-3">
-                        <label for="direccion" class="col-form-label my-2">DIRECCION</label>
-                    </div>
-                    <div class="col-md-5">
-                        <input type="text" name="direccion" id="direccion" class="form-control text-center my-2"
-                               value="<?= $cliente->__get("direccion")?>"
-                               placeholder="Ingrese direccion">
-                    </div>
+                <div class="col-md-6">
+                    <label for="celular" class="col-form-label">Celular</label>
+                    <input type="tel" name="celular" id="celular" value="<?= $cliente->__get("celular")?>" class="form-control" placeholder="Ingrese Celular"
+                           pattern="[0-9]{7-10}" minlength="7" maxlength="10" title="Debe contener minimo 7 digitos, maximo 10">
+                </div>
+
+                <div class="col-md-6">
+                    <label for="direccion" class="col-form-label">Direccion</label>
+                    <input type="text" name="direccion" id="direccion" value="<?= $cliente->__get("direccion")?>" class="form-control" placeholder="Ingrese Direccion"
+                           required>
                 </div>
 
 
 
-                <div class=" form-group row offset-md-2 my-0">
-                    <div class="col-md-3">
-                        <label for="email" class="col-form-label my-2">E-MAIL</label>
-                    </div>
-                    <div class="col-md-5">
-                        <input type="text" name="email" id="email" class="form-control text-center my-2"
-                               value="<?= $cliente->__get("email")?>"
-                               placeholder="Ingrese e-mail">
-                    </div>
+                <div class="col-md-8 m-auto pt-4">
+                    <button type="submit" class="btn btn-primary btn-block">Actualizar</button>
                 </div>
 
-                <div class="form-group row offset-md-2 my-2 mt-4">
-                    <div class="col-md-8 ">
-                        <button class="btn btn-lg btn-primary btn-block" type="submit" name="boton">Actualizar</button>
-                    </div>
-                </div>
 
             </form>
 
             <footer class="row offset-md-2 text-center">
-                <p class="mt-5 mb-3 text-muted col-md-8">&copy;2020</p>
+                <p class="mt-5 mb-3 text-muted col-md-10">&copy;2020</p>
             </footer>
 
         </main>
+
     </div>
 
 </div>
