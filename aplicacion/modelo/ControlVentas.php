927303466548c1685e8ea6562a444074541945cd
Faver <faverl@misena.edu.co>
2021-03-06 00:58:43 -0500

    Se corrigen errores en el modelo

--- aplicacion/modelo/ControlVentas.php
+++ aplicacion/modelo/ControlVentas.php
@@ -1,5 +1,11 @@
 <?php
 
+namespace ControlVentas;
+
+use Conexion;
+use Exception;
+use PDO;
+use servicio\Servicio;
 
 class ControlVentas
 {
@@ -14,272 +20,184 @@ class ControlVentas
     private $numero_orden_instalacion;
     private $pdo;
 
-    /**
-     * ControlVentas constructor.
-     * @param $id_ventas
-     * @param $oferta
-     * @param $asesor
-     * @param $cliente
-     * @param $servicio
-     * @param $estado
-     * @param $fecha
-     * @param $numero_orden_instalacion
-     */
-    /*public function __construct(
-        $id_ventas,
-        $oferta,
-        $asesor,
-        $cliente,
-        $servicio,
-        $estado,
-        $fecha,
-        $numero_orden_instalacion
-    ) {
-        $this->id_ventas = $id_ventas;
-        $this->oferta = $oferta;
-        $this->asesor = $asesor;
-        $this->cliente = $cliente;
-        $this->servicio = $servicio;
-        $this->estado = $estado;
-        $this->fecha = $fecha;
-        $this->numero_orden_instalacion = $numero_orden_instalacion;
-    }*/
 
-    /**
-     * @return mixed
-     */
     public function getIdVentas()
     {
         return $this->id_ventas;
     }
 
-    /**
-     * @param mixed $id_ventas
-     */
     public function setIdVentas($id_ventas): void
     {
         $this->id_ventas = $id_ventas;
     }
 
-    /**
-     * @return mixed
-     */
     public function getOferta()
     {
         return $this->oferta;
     }
 
-    /**
-     * @param mixed $oferta
-     */
     public function setOferta($oferta): void
     {
         $this->oferta = $oferta;
     }
 
-    /**
-     * @return mixed
-     */
     public function getAsesor()
     {
         return $this->asesor;
     }
 
-    /**
-     * @param mixed $asesor
-     */
     public function setAsesor($asesor): void
     {
         $this->asesor = $asesor;
     }
 
-    /**
-     * @return mixed
-     */
     public function getCliente()
     {
         return $this->cliente;
     }
 
-    /**
-     * @param mixed $cliente
-     */
     public function setCliente($cliente): void
     {
         $this->cliente = $cliente;
     }
 
-    /**
-     * @return mixed
-     */
     public function getServicio()
     {
         return $this->servicio;
     }
 
-    /**
-     * @param mixed $servicio
-     */
     public function setServicio($servicio): void
     {
         $this->servicio = $servicio;
     }
 
-    /**
-     * @return mixed
-     */
     public function getEstado()
     {
         return $this->estado;
     }
 
-    /**
-     * @param mixed $estado
-     */
     public function setEstado($estado): void
     {
         $this->estado = $estado;
     }
 
-    /**
-     * @return mixed
-     */
     public function getFecha()
     {
         return $this->fecha;
     }
 
-    /**
-     * @param mixed $fecha
-     */
     public function setFecha($fecha): void
     {
         $this->fecha = $fecha;
     }
 
-    /**
-     * @return mixed
-     */
     public function getNumeroOrdenInstalacion()
     {
         return $this->numero_orden_instalacion;
     }
 
-    /**
-     * @param mixed $numero_orden_instalacion
-     */
     public function setNumeroOrdenInstalacion($numero_orden_instalacion): void
     {
         $this->numero_orden_instalacion = $numero_orden_instalacion;
     }
-    /**
-     * crud Class ControlVentas
-     */
 
     public function listar()
     {
-        try
-        {
+        try {
             $result = array();
 
-            $stm = $this->pdo->prepare("SELECT * FROM control_ventas");
+            $conexionDataBase = new Conexion();
+            $conexion = $conexionDataBase->CrearConexion();
+
+            $stm = $conexion->prepare("SELECT * FROM control_ventas");
             $stm->execute();
 
-            foreach($stm->fetchAll(PDO::FETCH_OBJ) as $r)
-            {
-                $clt = new cvtas();
+            foreach ($stm->fetchAll(PDO::FETCH_OBJ) as $r) {
 
-                $cvtas->__SET('id', $r->id);
-                $cvtas->__SET('oferta', $r->oferta);
-                $cvtas->__SET('asesor', $r->asesor);
-                $cvtas->__SET('cliente', $r->cliente);
-                $cvtas->__SET('servicio', $r->servicio);
-                $cvtas->__SET('estado', $r->estado);
-                $cvtas->__SET('fecha', $r->fecha);
-                $cvtas->__SET('numero_orden_instalacion', $r->numero_orden_instalacion);
+                $cvtas = new ControlVentas();
+
+                $cvtas->setIdVentas($r->id);
+                $cvtas->setOferta($r->oferta);
+                $cvtas->setAsesor($r->asesor);
+                $cvtas->setCliente($r->cliente);
+                $cvtas->setServicio($r->servicio);
+                $cvtas->setEstado($r->estado);
+                $cvtas->setFecha($r->fecha);
+                $cvtas->setNumeroOrdenInstalacion($r->numero_orden_instalacion);
 
                 $result[] = $cvtas;
             }
 
             return $result;
-        }
-        catch(Exception $e)
-        {
+        } catch (Exception $e) {
             die($e->getMessage());
         }
     }
 
     public function Obtener($id)
     {
-        try
-        {
-            $stm = $this->pdo
-                      ->prepare("SELECT * FROM control_ventas WHERE id = ?");
+        try {
+            $conexionDataBase = new Conexion();
+            $conexion = $conexionDataBase->CrearConexion();
+
+            $stm = $conexion->prepare("SELECT * FROM control_ventas WHERE id = ?");
 
             $stm->execute(array($id));
             $r = $stm->fetch(PDO::FETCH_OBJ);
 
-            $clt = new cvtas();
+            $cvtas = new ControlVentas();
 
-            $clt->__SET('id', $r->id);
-            $cvtas->__SET('oferta', $r->oferta);
-            $cvtas->__SET('asesor', $r->asesor);
-            $cvtas->__SET('cliente', $r->cliente);
-            $cvtas->__SET('servicio', $r->servicio);
-            $cvtas->__SET('estado', $r->estado);
-            $cvtas->__SET('fecha', $r->fecha);
-            $cvtas->__SET('numero_orden_instalacion', $r->numero_orden_instalacion);
+            $cvtas->setIdVentas($r->id);
+            $cvtas->setOferta($r->oferta);
+            $cvtas->setAsesor($r->asesor);
+            $cvtas->setCliente($r->cliente);
+            $cvtas->setServicio($r->servicio);
+            $cvtas->setEstado($r->estado);
+            $cvtas->setFecha($r->fecha);
+            $cvtas->setNumeroOrdenInstalacion($r->numero_orden_instalacion);
 
             return $cvtas;
-        } catch (Exception $e)
-        {
+        } catch (Exception $e) {
             die($e->getMessage());
         }
     }
 
     public function Eliminar($id)
     {
-        try
-        {
-            $stm = $this->pdo
-                      ->prepare("DELETE FROM control_ventas WHERE id = ?");
+        try {
+            $conexionDataBase = new Conexion();
+            $conexion = $conexionDataBase->CrearConexion();
+
+            $stm = $conexion->prepare("DELETE FROM control_ventas WHERE id = ?");
 
             $stm->execute(array($id));
-        } catch (Exception $e)
-        {
+        } catch (Exception $e) {
             die($e->getMessage());
         }
     }
 
     public function Actualizar(ControlVentas $data)
     {
-        try
-        {
-            $sql = "UPDATE control_ventas SET
-                        oferta
-                        asesor         = ?,
-                        cliente       = ?,
-                        servicio           = ?,
-                        estado        = ?,
-                        fecha           = ?,
-                        numero_orden_instalacion        = ?
+        try {
+            $sql = "UPDATE control_ventas 
+                    SET oferta=?, fk_usuario=?, fk_cliente=?, fk_servicio=?, fk_estado=?,fecha=?, numero_instalacion=?
                     WHERE id = ?";
 
-            $this->pdo->prepare($sql)
-                 ->execute(
-                array(
-                    $data->__GET('oferta'),
-                    $data->__GET('asesor'),
-                    $data->__GET('cliente'),
-                    $data->__GET('servicio'),
-                    $data->__GET('estado'),
-                    $data->__GET('fecha '),
-                    $data->__GET('numero_orden_instalacion'),
+            $conexionDataBase = new Conexion();
+            $conexion = $conexionDataBase->CrearConexion();
 
-                ));
+            $stm = $conexion->prepare($sql);
+            $stm->execute(array(
+                $data->getOferta(),
+                $data->getAsesor(),
+                $data->getCliente(),
+                $data->getServicio(),
+                $data->getEstado(),
+                $data->getFecha(),
+                $data->getNumeroOrdenInstalacion()
+            ));
 
-        } catch (Exception $e)
-        {
+        } catch (Exception $e) {
             die($e->getMessage());
         }
     }
@@ -287,67 +205,59 @@ class ControlVentas
 
     public function Registrar(ControlVentas $data)
     {
-        try
-        {
-        $sql = "INSERT INTO control_ventas (id, oferta, asesor,cliente, servicio,estado,fecha,numero_orden_instalacion)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        try {
+            $sql = "INSERT INTO control_ventas (oferta, fk_usuario, fk_cliente, fk_servicio,fk_estado,fecha,numero_instalacion)
+                    VALUES (?, ?, ?, ?, ?, ?, ?)";
 
-        $this->pdo->prepare($sql)
-             ->execute(
-            array(
-                $data->__GET('id'),
-                $data->__GET('oferta'),
-                $data->__GET('asesor,'),
-                $data->__GET('cliente'),
-                $data->__GET('servicio'),
-                $data->__GET('estado'),
-                $data->__GET('fecha'),
-                $data->__GET('numero_orden_instalacion'),
+            $conexionDataBase = new Conexion();
+            $conexion = $conexionDataBase->CrearConexion();
 
-
-                )
-            );
-        } catch (Exception $e)
-        {
+            $stm = $conexion->prepare($sql);
+            $stm->execute(array(
+                $data->getOferta(),
+                $data->getAsesor(),
+                $data->getCliente(),
+                $data->getServicio(),
+                $data->getEstado(),
+                $data->getFecha(),
+                $data->getNumeroOrdenInstalacion()
+            ));
+        } catch (Exception $e) {
             die($e->getMessage());
         }
-      }
+    }
 
-      public function ListarControlVentasDelCliente($id)
-      {
+    public function ListarControlVentasDelCliente($id)
+    {
         $ListaSeguimiento = array();
-        try
-        {
-            $stm = $this->pdo
-                      ->prepare("SELECT control_ventas.id, servicios.id_servicio, servicios.tipo_servicio
-                        AS servicio, control_ventas.fecha
-                        FROM control_ventas
-                        INNER JOIN servicios
-                        ON control_ventas.fk_servicio = servicios.id_servicios
-                        WHERE control_ventas.fk_cliente = ?");
+        try {
+            $conexionDataBase = new Conexion();
+            $conexion = $conexionDataBase->CrearConexion();
 
+            $stm = $conexion->prepare("SELECT control_ventas.id, servicios.id_servicios, servicios.tipo_servicio AS servicio, control_ventas.fecha 
+                           FROM control_ventas
+                           INNER JOIN servicios ON control_ventas.fk_servicio = servicios.id_servicios
+                           WHERE control_ventas.fk_cliente = ?");
+            $stm->setFetchMode(PDO::FETCH_OBJ);
             $stm->execute(array($id));
-            $r = $stm->fetch(PDO::FETCH_OBJ);
 
-            foreach ($r as $seguimiento) {
-              $control = new ControlVentas();
+            while ($seguimiento = $stm->fetch()) {
 
-              $control->setIdVentas($seguimiento->id);
+                $control = new ControlVentas();
 
-              $ser = new servicio();
-              $ser->__SET('id', $seguimiento->id_servicio);
-              $ser->__SET('tipo_servicio', $seguimiento->servicio);
+                $control->setIdVentas($seguimiento->id);
 
-              $control->setServicio($ser);
-              $control->setFecha($seguimiento->fecha);
-              $ListaSeguimiento[] = $control;
+                $ser = new Servicio($seguimiento->id_servicios, $seguimiento->servicio);
+
+                $control->setServicio($ser);
+                $control->setFecha($seguimiento->fecha);
+                $ListaSeguimiento[] = $control;
 
             }
 
             return $ListaSeguimiento;
-        } catch (Exception $e)
-        {
+        } catch (Exception $e) {
             die($e->getMessage());
         }
-      }
+    }
 }
