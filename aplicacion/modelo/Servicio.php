7333c35bf3bd75771edc8a05c5f1af39c81cf31f
Faver <faverl@misena.edu.co>
2021-03-06 00:59:31 -0500

    Se corrigen errores en el modelo

--- aplicacion/modelo/Servicio.php
+++ aplicacion/modelo/Servicio.php
@@ -1,163 +1,130 @@
 <?php
 
+namespace servicio;
+
+use Conexion;
+use Exception;
+use PDO;
 
 class Servicio
 {
     private $id_servicio;
     private $tipo_servicio;
-    private $pdo;
 
-    /**
-     * Servicio constructor.
-     * @param $id_servicio
-     * @param $tipo_servicio
-     */
+
     public function __construct($id_servicio, $tipo_servicio)
     {
         $this->id_servicio = $id_servicio;
         $this->tipo_servicio = $tipo_servicio;
     }
 
-    /**
-     * @return mixed
-     */
     public function getIdServicio()
     {
         return $this->id_servicio;
     }
 
-    /**
-     * @param mixed $id_servicio
-     */
     public function setIdServicio($id_servicio): void
     {
         $this->id_servicio = $id_servicio;
     }
 
-    /**
-     * @return mixed
-     */
     public function getTipoServicio()
     {
         return $this->tipo_servicio;
     }
 
-    /**
-     * @param mixed $tipo_servicio
-     */
     public function setTipoServicio($tipo_servicio): void
     {
         $this->tipo_servicio = $tipo_servicio;
     }
-    /**
-     * crud Class servicio
-     */
 
     public function listar()
     {
-        try
-        {
+        try {
+
             $result = array();
 
-            $stm = $this->pdo->prepare("SELECT * FROM servicios");
+            $conexionDataBase = new Conexion();
+            $conexion = $conexionDataBase->CrearConexion();
+
+            $stm = $conexion->prepare("SELECT * FROM servicios");
             $stm->execute();
 
-            foreach($stm->fetchAll(PDO::FETCH_OBJ) as $r)
-            {
-                $ser = new servicio();
+            foreach ($stm->fetchAll(PDO::FETCH_OBJ) as $r) {
 
-                $ser->__SET('id', $r->id);
-                $ser->__SET('tipo_servicio', $r->tipo_servicio);
+                $ser = new servicio($r->id, $r->tipo_servicio);
 
                 $result[] = $ser;
             }
 
             return $result;
-        }
-        catch(Exception $e)
-        {
+        } catch (Exception $e) {
             die($e->getMessage());
         }
     }
 
-    public function Obtener($id)
+    public static function Obtener($id)
     {
-        try
-        {
-            $stm = $this->pdo
-                      ->prepare("SELECT * FROM servicios WHERE id = ?");
+        try {
+
+            $conexionDataBase = new Conexion();
+            $conexion = $conexionDataBase->CrearConexion();
+
+            $stm = $conexion->prepare("SELECT * FROM servicios WHERE id_servicios = ?");
 
             $stm->execute(array($id));
             $r = $stm->fetch(PDO::FETCH_OBJ);
 
-            $ser = new servicio();
+            return new servicio($r->id, $r->tipo_servicio);
 
-            $ser->__SET('id', $r->id);
-            $ser->__SET('tipo_servicio', $r->tipo_servicio);
-
-
-            return $ser;
-        } catch (Exception $e)
-        {
+        } catch (Exception $e) {
             die($e->getMessage());
         }
     }
 
     public function Eliminar($id)
     {
-        try
-        {
-            $stm = $this->pdo
-                      ->prepare("DELETE FROM servicios WHERE id = ?");
+        try {
+            $conexionDataBase = new Conexion();
+            $conexion = $conexionDataBase->CrearConexion();
+
+            $stm = $conexion->prepare("DELETE FROM servicios WHERE id_servicios = ?");
 
             $stm->execute(array($id));
-        } catch (Exception $e)
-        {
+        } catch (Exception $e) {
             die($e->getMessage());
         }
     }
 
     public function Actualizar(servicio $data)
     {
-        try
-        {
-            $sql = "UPDATE servicio SET
-                        tipo_servicio
-                    WHERE id = ?";
+        try {
+            $sql = "UPDATE servicios SET tipo_servicio = ? WHERE id_servicios = ?";
 
-            $this->pdo->prepare($sql)
-                 ->execute(
-                array(
-                    $data->__GET('tipo_servicio'),
-                ));
+            $conexionDataBase = new Conexion();
+            $conexion = $conexionDataBase->CrearConexion();
 
-        } catch (Exception $e)
-        {
+            $stm = $conexion->prepare($sql);
+            $stm->execute(array($data->getTipoServicio(), $data->getIdServicio()));
+
+        } catch (Exception $e) {
             die($e->getMessage());
         }
     }
 
-
     public function Registrar(servicio $data)
     {
-        try
-        {
-        $sql = "INSERT INTO servicio (id, tipo_servicio)
+        try {
+            $sql = "INSERT INTO servicios (id_servicios, tipo_servicio)
                 VALUES (?, ?)";
 
-        $this->pdo->prepare($sql)
-             ->execute(
-            array(
-                $data->__GET('id'),
-                $data->__GET('tipo_servicio'),
+            $conexionDataBase = new Conexion();
+            $conexion = $conexionDataBase->CrearConexion();
 
-
-
-                )
-            );
-        } catch (Exception $e)
-        {
+            $stm = $conexion->prepare($sql);
+            $stm->execute(array($data->getIdServicio(), $data->getTipoServicio()));
+        } catch (Exception $e) {
             die($e->getMessage());
         }
-}
+    }
 }
