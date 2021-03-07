cc88963fb78db52a451b3571353e581810000f2f
Juan Carlos <jhcarlos181@protonmail.com>
2021-03-07 12:35:54 -0500

    Se agrega ruta de fuente google fonts en el archivo cabecera, se agrega lineas de fuente en estilos.css, se borra ruta en git ignore que ya no estoy utilizando

--- vistas/partes/head.php
+++ vistas/partes/head.php
@@ -2,5 +2,10 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+<!--Fuentes para las paginas-->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet">
+
+<!--Rutas de CSS-->
 <link href="<?php assets("css/bootstrap/bootstrap.css" )?>" type="text/css" rel="stylesheet">
 <link href="<?php assets("css/estilo.css")?>" type="text/css" rel="stylesheet">
