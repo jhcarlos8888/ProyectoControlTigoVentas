4ce50f91a80342f3798a7abe351960fe44db2519
Faver <faverl@misena.edu.co>
2021-03-07 17:18:44 -0500

    Se agrega url y realiza cambio de la opcion de volantes a mi gestion

--- vistas/partes/menu.php
+++ vistas/partes/menu.php
@@ -6,25 +6,22 @@
 
             <li class="nav-item">
                 <a class="nav-link active" href="#">
-
                     <span data-feather="airplay"></span>¡Hola!
-                    <span><?php echo (isset($_SESSION['nombre_user'])) ? $_SESSION['nombre_user'] : "Error User_name" ?></span>
+                    <span>
+                        <?php echo (isset($_SESSION['nombre_user'])) ? $_SESSION['nombre_user'] : "Error User_name" ?>
+                    </span>
                 </a>
             </li>
 
             <li class="nav-item">
-
-                <a class="nav-link" role="button" href="<?php url("usuario") ?>" ><span data-feather="users"></span>Usuario</a>
-
-
+                <a class="nav-link" role="button" href="<?php url("usuario") ?>">
+                    <span data-feather="users"></span>Usuario</a>
             </li>
 
-
             <li class="nav-item dropdow">
-
-                <a class="nav-link dropdown-toggle" role="button" href="<?php url("cliente") ?>" data-toggle="collapse"
-                   data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne"><span
-                            data-feather="bar-chart-2"></span>Clientes</a>
+                <a class="nav-link dropdown-toggle" role="button" href="<?php url("cliente") ?>"
+                   data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                    <span data-feather="bar-chart-2"></span>Clientes</a>
 
                 <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
                     <a class="dropdown-item" href="<?php url("cliente") ?>">Seguimiento de clientes</a>
@@ -34,10 +31,9 @@
             </li>
 
             <li class="nav-item dropdown">
-
                 <a class="nav-link dropdown-toggle" role="button" href="#" data-toggle="collapse"
-                   data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"><span
-                            data-feather="dollar-sign"></span>Mi gestión</a>
+                   data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <span data-feather="dollar-sign"></span>Mi gestión</a>
 
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
                     <a class="dropdown-item" href="#">Descuentos y promociones</a>
@@ -45,18 +41,17 @@
                     <a class="dropdown-item" href="#">Estrategias de ventas</a>
                     <a class="dropdown-item" href="#">Generación de informes financieros</a>
                     <a class="dropdown-item" href="#">Indicadores de gestión comercial</a>
+                    <a class="dropdown-item" href="<?php url("volante") ?>">Volantes</a>
                 </div>
             </li>
 
             <li class="nav-item dropdown">
-
                 <a class="nav-link dropdown-toggle" role="button" href="#" data-toggle="collapse"
                    data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"><span
                             data-feather="help-circle"></span>Ayudas</a>
 
                 <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
                     <a class="dropdown-item" href="<?php url("manuales") ?>">Manuales</a>
-                    <a class="dropdown-item" href="#">Volantes</a>
                 </div>
             </li>
 
