44fec34a0ee23ab50341eb0cc1e87fde055b415b
Faver <faverl@misena.edu.co>
2021-03-07 21:55:37 -0500

    Se realizan cambios en la vistas +- Se cambia la barra de busqueda ++ Se agregan datos a la tabla de seguimiento +- Se modifica los botones de Añadir y regresar

--- vistas/Clientes/servicio/ActualizarServicio.php
+++ vistas/Clientes/servicio/ActualizarServicio.php
@@ -16,33 +16,24 @@
 
           <?php include(RUTA_VISTAS . 'partes/menu.php') ?>
 
-          <main role="main" class="col-12 col-md-10 px-md-4 py-md-5 py-2">
+          <main role="main" class="col-12 col-md-10 px-md-4 py-md-2 py-2">
               <div class="row">
                   <div class="col-md-4 py-md-3 py-2 order-2 order-md-1">
-                      <a class="btn btn-primary w-100" href="<?php url("seguimiento/registrar/") ?>"
+                      <a class="btn btn-primary w-40" href="<?php url("seguimiento/registrar") ?>"
                          role="button">Añadir producto</a>
-                       <br>
-                      <a class="btn btn-primary w-100" href="<?php url("") ?>"
-                            role="button">Cancelar</a>
+                      <a class="btn btn-danger w-40" href="<?php url("") ?>"
+                            role="button">Regresar</a>
                   </div>
 
                   <div class="offset-md-2 col-md-6 offset-lg-4 col-lg-4 py-md-3 order-1 order-md-2">
                       <div class="input-group">
-                          <input type="text" id="busqueda" placeholder="Buscar Cliente" class="form-control" data-generator="Cliente"
+                          <input type="text" id="busqueda" placeholder="Buscar Cliente" class="form-control"
+                                 data-generator="seguimiento"
                                  aria-label="Text input with segmented dropdown button">
                           <div class="input-group-append">
                               <button type="button" class="btn btn-outline-secondary">
-                                  <span data-feather="filter"></span>
+                                  <span data-feather="search"></span>
                               </button>
-                              <button type="button"
-                                      class="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split"
-                                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                      <span class="sr-only">Toggle Dropdown</span>
-                              </button>
-                              <div class="dropdown-menu">
-                                  <a class="dropdown-item" href="#">Cedula</a>
-                                  <a class="dropdown-item" href="#">servicios</a>
-                              </div>
                           </div>
                       </div>
                   </div>
@@ -51,9 +42,13 @@
 
                       <table class="table table-hover" id="tblServices" aria-label="Lista de Servicios">
                           <thead>
+                              <tr class="text-center bg-info">
+                                  <th scope="col" COLSPAN="4"><h4><?= $cliente->__get("nombre") ?></h4></th>
+                              </tr>
                               <tr>
                                   <th scope="col">TIPO DE PROCESO</th>
                                   <th scope="col">FECHA</th>
+                                  <th scope="col">ESTADO</th>
                                   <th scope="col">ACCIONES</th>
                                 </tr>
                             </thead>
@@ -64,6 +59,7 @@
                                       <tr>
                                           <td><?= $Listado[$i]->getServicio()->getTipoServicio(); ?></td>
                                           <td><?= $Listado[$i]->getFecha(); ?></td>
+                                          <td><?= $Listado[$i]->getEstado()->getEstado(); ?></td>
                                           <td>
                                               <a href="#"
                                                  class="bnt"><span data-feather="eye"></span></a>
@@ -71,7 +67,6 @@
                                                  class="bnt"><span data-feather="edit-3"></span></a>
                                               <a href="<?php url("seguimiento/eliminar/" . $Listado[$i]->getIdVentas()) ?>"
                                                  class="bnt"><span data-feather="trash-2"></span></a>
-
                                           </td>
                                       </tr>
                                   <?php }
