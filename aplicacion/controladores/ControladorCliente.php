36e0834f355beb20b5a304c1414e75b4ee94d864
Faver Lopez <faverlpz@gmail.com>
2021-04-03 21:03:49 -0500

    Se agrega funcion para realizar valdiacion de rol

--- aplicacion/controladores/ControladorCliente.php
+++ aplicacion/controladores/ControladorCliente.php
@@ -9,37 +9,29 @@ class ControladorCliente
     public function index()
     {
         validarSession();
-
         $cliente = new Cliente();
-
         $listaClientes = $cliente->listar();
-
         return Vista::crear("Clientes.ListarClientes", "ListarClientes", $listaClientes);
     }
 
     public function editar($id)
     {
         validarSession();
-
         $cliente = new cliente();
         $cliente = $cliente->Obtener($id);
-
         return Vista::crear("Clientes.actualizar", "cliente", $cliente);
     }
 
     public function actualizar()
     {
         validarSession();
-
         $id = $_POST['id'];
         $identificacion = $_POST['identificacion'];
         $nombre = $_POST['nombre'];
         $celular = $_POST['celular'];
         $direccion = $_POST['direccion'];
         $email = $_POST['email'];
-
         $cliente = new cliente();
-
         $cliente->__set("id",$id);
         $cliente->__set("identificacion", $identificacion);
         $cliente->__set("nombre", $nombre);
@@ -59,21 +51,17 @@ class ControladorCliente
     public function crear()
     {
         validarSession();
-
         $identificacion = $_POST['identificacion'];
         $nombre = $_POST['nombre'];
         $celular = $_POST['celular'];
         $direccion = $_POST['direccion'];
         $email = $_POST['email'];
-
         $cliente = new cliente();
-
         $cliente->__set("identificacion", $identificacion);
         $cliente->__set("nombre", $nombre);
         $cliente->__set("celular", $celular);
         $cliente->__set("direccion", $direccion);
         $cliente->__set("email", $email);
-
         $resultado = $cliente->Registrar();
 
         if ($resultado) {
@@ -87,17 +75,22 @@ class ControladorCliente
     public function registrar()
     {
         validarSession();
-
         return Vista::crear("Clientes.registrar");
     }
 
     public function eliminar($id)
     {
         validarSession();
-
+		$this->validarRol();
         $cliente = new cliente();
         $cliente->Eliminar($id);
-
         redirecciona("cliente");
     }
+
+	private function validarRol(){
+		if($_SESSION['rol_user'] !== "Administrador" || $_SESSION['rol_user'] !== "Coordinador Comercial"){
+			redirecciona("");
+			exit();
+		}
+	}
 }
